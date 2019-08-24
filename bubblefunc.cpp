7eef5201b714df5b3fc835e330f6cd11625828be
Os <os@osmancampeon69@gmail.com>
2019-08-23 21:34:18 -0500

    fin

--- bubblefunc.cpp
+++ bubblefunc.cpp
@@ -1,28 +1,54 @@
 /*------DECLARATIONS------*/
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
 /*_bubblesort_ con punteros a funcion*/
-void * _bubblesort_(void * _array_, void (*_puntero_) (void *, int _i, int _k)){
+void * _bubblesort_(void *_array_, void (*_puntero_) (void *, int _i, int _k),int _tam_){
 	int _k = 0;
 	for (int i = 0; i < _tam_ ; i ++)
 		for(int k = 0; k < _tam_-1; k++)
-			_puntero_(_array_,k,_k);
+			_puntero_(_array_,k,_k); //la _k es innecesaria
+		
 		return _array_;
 }
 
 /*------FUNCTIONS------*/
-
+//la _k es innecesaria en cada caso
 void  _b_int_ (void * _vector_, int _i,int _k){
+	if ( ((int*) _vector_)[_i] > ((int*) _vector_)[_i+1])
+            {
+                int temp1 = ((int*) _vector_)[_i+1];
+                ((int*) _vector_)[_i+1]=((int*) _vector_)[_i];
+                ((int*) _vector_)[_i]=temp1;
+            }       
 }
 
 void  _b_float_ (void * _vector_, int _i,int _k){
+	if ( ((float*) _vector_)[_i] > ((float*) _vector_)[_i+1])
+            {
+                float temp1 = ((float*) _vector_)[_i+1];
+                ((float*) _vector_)[_i+1]=((float*) _vector_)[_i];
+                ((float*) _vector_)[_i]=temp1;
+            }   
 }
 
 void  _b_double_ (void * _vector_, int _i,int _k){
+	if ( ((double*) _vector_)[_i] > ((double*) _vector_)[_i+1])
+            {
+                double temp1 = ((double*) _vector_)[_i+1];
+                ((double*) _vector_)[_i+1]=((double*) _vector_)[_i];
+                ((double*) _vector_)[_i]=temp1;
+            }  
 }
 
 void  _b_char_ (void * _vector_, int _i,int _k){
+	if ( ((char*) _vector_)[_i] > ((char*) _vector_)[_i+1])
+            {
+                char temp1 = ((char*) _vector_)[_i+1];
+                ((char*) _vector_)[_i+1]=((char*) _vector_)[_i];
+                ((char*) _vector_)[_i]=temp1;
+            }  
 }
 
 
@@ -35,10 +61,10 @@ int main(){
 	double _array_3 [5] = {3.0,8.0,2.0,4.0,15.0};
 	char _array_4 [6] = {'k','a','r','l','o','s'};
 
-	_bubblesort_(_array_1,_b_int_);	
-	_bubblesort_(_array_2,_b_float_);
-	_bubblesort_(_array_3,_b_double_);
-	_bubblesort_(_array_4,_b_char_);
+	_bubblesort_(_array_1,_b_int_,5);	
+	_bubblesort_(_array_2,_b_float_,5);
+	_bubblesort_(_array_3,_b_double_,5);
+	_bubblesort_(_array_4,_b_char_,6);
     return 0;
 }
 
